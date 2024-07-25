433da89370055167ce73fdf18784ea4be1a94673
Levi375 <demorais.kt@gmail.com>
2024-07-25 05:51:15 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+function f1() {
+	let m1 = document.querySelector("#menu1");
+	let m2 = document.querySelector("#menu2");
+		m1.style.display = "none";
+		m2.style.display = "inline-flex";	
+}
+
+function f2() {
+	let m1 = document.querySelector("#menu1");
+	let m2 = document.querySelector("#menu2");
+		m2.style.display = "none";
+		m1.style.display = "inline-flex";	
+}
+
+
+	
+
+
+
+
