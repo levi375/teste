65030cdd45bf5fd920d0e15da33fc47369494b70
Levi375 <demorais.kt@gmail.com>
2024-07-30 10:37:13 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,12 @@
-function f1() {
-	let m1 = document.querySelector("#menu1");
-	let m2 = document.querySelector("#menu2");
-		m1.style.display = "none";
-		m2.style.display = "inline-flex";	
+const menu = document.querySelector('.menu');
+
+function direita(){                 
+	menu.style.transform = "translate(-50%)";
+	
 }
 
-function f2() {
-	let m1 = document.querySelector("#menu1");
-	let m2 = document.querySelector("#menu2");
-		m2.style.display = "none";
-		m1.style.display = "inline-flex";	
+function esquerda(){    
+	menu.style.transform = "translate(0px)";
 }
 
 
