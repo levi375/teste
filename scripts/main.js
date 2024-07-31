5a08086456ec24dc0aef23a80095e72d909f762c
Levi375 <demorais.kt@gmail.com>
2024-07-31 15:28:04 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,57 @@
 const menu = document.querySelector('.menu');
+const quantidade_de_icones = 12;
+const icones_visiveis = 6;
+const largura_a_percorrer = (icones_visiveis / quantidade_de_icones) * 100;
+const cliques_possiveis = quantidade_de_icones / icones_visiveis;
+let repetições = 0;
 
-function direita(){                 
-	menu.style.transform = "translate(-50%)";
+if (window.innerWidth <= 600) {
+
+	const quantidade_de_icones = 12;
+	const icones_visiveis = 4;
+	const largura_a_percorrer = (icones_visiveis / quantidade_de_icones) * 100;
+	const cliques_possiveis = quantidade_de_icones / icones_visiveis;
 	
+	function direita() {
+    repetições++;
+    
+		if (repetições >= cliques_possiveis) {
+			repetições = cliques_possiveis - 1;
+		}
+		
+		menu.style.transform = `translate(-${largura_a_percorrer * repetições}%)`;
+	}
+
+	function esquerda() {
+		repetições--;
+		
+		if (repetições < 0) {
+			repetições = 0;
+		}
+		
+		menu.style.transform = `translate(-${largura_a_percorrer * repetições}%)`;
+	}
+
 }
 
-function esquerda(){    
-	menu.style.transform = "translate(0px)";
+function direita() {
+    repetições++;
+    
+    if (repetições >= cliques_possiveis) {
+        repetições = cliques_possiveis - 1;
+    }
+    
+    menu.style.transform = `translate(-${largura_a_percorrer * repetições}%)`;
 }
 
-
-	
-
-
+function esquerda() {
+    repetições--;
+    
+    if (repetições < 0) {
+        repetições = 0;
+    }
+    
+    menu.style.transform = `translate(-${largura_a_percorrer * repetições}%)`;
+}
 
 
